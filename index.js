cf14aa9d4edb02d3dc41d9d1fb803da6a6b07371
SnubStumpy2007 <joshua192007@gmail.com>
2023-07-13 17:04:31 -0400

    finally got it to work.  Probably does not meet full requirements but at least I learned something

--- index.js
+++ index.js
@@ -1,85 +1,78 @@
 const inquirer = require('inquirer');
 const { createSVGWindow } = require('svgdom');
-const window = createSVGWindow()
-const SVG = require('svg.js')(window)
-const document = window.document
-
-// const canvas = SVG(document.documentElement)
+const window = createSVGWindow();
+const SVG = require('svg.js')(window);
 
 const fs = require('fs');
 const path = require('path');
 
 function generateSVG(text, textColor, shape, shapeColor) {
-    const svg = SVG().size(300, 200); // Create an SVG container with a specified size
-  
-    // Create a text element
-    const textElement = svg.text(text).move(50, 100);
-    textElement.fill(textColor);
-  
-    // Create a shape element based on the chosen shape
-    let shapeElement;
-    switch (shape) {
-      case 'Circle':
-        shapeElement = svg.circle(100);
-        break;
-      case 'Triangle':
-        shapeElement = svg.polygon('0,100 50,0 100,100');
-        break;
-      case 'Square':
-        shapeElement = svg.rect(100, 100);
-        break;
-      default:
-        break;
-    }
-    shapeElement.move(150, 50).fill(shapeColor);
-  
-    // Return the SVG code as a string
-    return svg.svg();
+  const svgContainer = SVG(window.document.documentElement);
+  const svg = svgContainer.size(300, 200);
+
+  const textElement = svg.text(text).move(50, 100);
+  textElement.fill(textColor);
+
+  let shapeElement;
+  switch (shape) {
+    case 'Circle':
+      shapeElement = svg.circle(100);
+      break;
+    case 'Triangle':
+      shapeElement = svg.polygon('0,100 50,0 100,100');
+      break;
+    case 'Square':
+      shapeElement = svg.rect(100, 100);
+      break;
+    default:
+      break;
+  }
+  shapeElement.move(150, 50).fill(shapeColor);
+
+  return svgContainer.svg();
 }
 
-
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter Text (up to three characters)',
-            validate: input => input.length <= 3,
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter your choice of color using hexidecimal codes or a keyword',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape from the list',
-            choices: ['Circle', 'Triangle', 'Square'],
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter the shape color (hexadecimal or keyword)',
-        },
-    ])
-.then(answers => {
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter Text (up to three characters)',
+      validate: input => input.length <= 3,
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter your choice of color using hexadecimal codes or a keyword',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape from the list',
+      choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (hexadecimal or keyword)',
+    },
+  ])
+  .then(answers => {
     const svgCode = generateSVG(
-        answers.text,
-        answers.textColor,
-        answers.shape,
-        answers.shapeColor
+      answers.text,
+      answers.textColor,
+      answers.shape,
+      answers.shapeColor
     );
 
-    // Save SVG code to file
     fs.writeFile('logo.svg', svgCode, err => {
-        if (err) {
-            console.log('Error saving file', err);
-        } else {
-            console.log('Success.');
-        }
+      if (err) {
+        console.log('Error saving file', err);
+      } else {
+        console.log('Success.');
+      }
     });
-})
-.catch(err => {
+  })
+  .catch(err => {
     console.log('Error saving', err);
-});
+  });
